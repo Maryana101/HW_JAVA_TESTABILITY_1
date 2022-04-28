1bc2f60bbd235039732ca589e8e01c73b376c812
Марьяна Варфоломеева <mihralievam@icloud.com>
2022-04-28 16:03:53 +0500

    исправлены тип и имя BONUS_FOR_MILE

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,9 @@
 public class BonusMilesService {
-    char BONUS_FOR_MILE = 20; // стоимость бонусной мили
+    int bonusForMile = 20; // стоимость бонусной мили
 
     public int calculate(int cost) {
 
-        return cost / BONUS_FOR_MILE;
+        return cost / bonusForMile;
     }
 
 }
